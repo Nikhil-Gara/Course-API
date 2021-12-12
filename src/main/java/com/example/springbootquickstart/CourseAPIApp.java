b2d3111f02f5ff235e6f24ffdea5bf4ac226cb48
Surya5286 <annakalyan286@gmail.com>
2021-12-12 16:52:19 +0530

    Step-1 -- SpringBoot part updated

---
+++ src/main/java/com/example/springbootquickstart/CourseAPIApp.java
@@ -0,0 +1,18 @@
+package com.example.springbootquickstart;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CourseAPIApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SpringApplication.run(CourseAPIApp.class, args);
+        
+	}
+
+}
